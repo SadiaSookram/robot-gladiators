2d4fcad7c6ec5f8f5fc183eb3ecb80c45c13c912
SadiaSookram <sadiamsookram@gmail.com>
2021-07-05 23:02:26 -0400

    bug fix name input complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,7 +75,7 @@ for (var i = 0; i < enemyInfo.length; i++) {
   if (playerInfo.health > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
+    
     // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyObj= enemyInfo[i];
 
@@ -167,9 +167,21 @@ switch (shopOptionPrompt) {
     return value;
   };
 
+  //function to set name
+  var getPlayerName = function() {
+    var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+
+  }
+  // add loop
+  console.log("Your robot's name is" + name);
+  return name;
+  };
+
   // What is your robot's name pop up and initial health input
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
